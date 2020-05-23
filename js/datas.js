82fe9edada56091c09b0469d62b1bc0ab343669f
Oláh Katalin <olahkatalin.inf@gmail.com>
2020-05-23 12:13:09 +0200

    Légitársaságok adatai datas.js

---
+++ js/datas.js
@@ -0,0 +1,148 @@
+[
+    {
+        nev : 'Endeavor Air Inc.',
+        osszes : 281625,
+        latogatott : 127,
+        torolt :  1,
+        atlagos : 79,
+        legforgalmassabb : "ATL"
+    },
+    {
+        nev : 'American Airlines Inc.',
+        osszes : 1018437,
+        latogatott : 117,
+        torolt :  2,
+        atlagos : 68,
+        legforgalmassabb : "DFW"
+    },
+    {
+        nev : 'Alaska Airlines Inc.',
+        osszes : 284697,
+        latogatott : 77,
+        torolt :  1,
+        atlagos : 51,
+        legforgalmassabb : "SEA"
+    },
+    {
+        nev : 'JetBlue Airways',
+        osszes : 321204,
+        latogatott : 70,
+        torolt :  1,
+        atlagos : 79,
+        legforgalmassabb : "BOS"
+    },
+    {
+        nev : 'Delta Air Lines Inc.',
+        osszes : 1073663,
+        latogatott : 154,
+        torolt :  0,
+        atlagos : 68,
+        legforgalmassabb : "ATL"
+    },
+    {
+        nev : 'ExpressJet Airlines Inc.',
+        osszes : 24389,
+        latogatott : 111,
+        torolt :  4,
+        atlagos : 80,
+        legforgalmassabb : "IAH"
+    },
+    {
+        nev : 'Frontier Airlines Inc.',
+        osszes : 149912,
+        latogatott : 106,
+        torolt :  2,
+        atlagos : 68,
+        legforgalmassabb : "BOS"
+    },
+    {
+        nev : 'Allegiant Air',
+        osszes :  115030,
+        latogatott : 126,
+        torolt :  1,
+        atlagos : 71,
+        legforgalmassabb : "SFB"
+    },
+    {
+        nev : 'Hawaiian Airlines Inc.',
+        osszes :  90637,
+        latogatott : 19,
+        torolt :  0,
+        atlagos : 40,
+        legforgalmassabb : "HNL"
+    },
+    {
+        nev : 'Envoy Air',
+        osszes :  351701,
+        latogatott : 169,
+        torolt :  3,
+        atlagos : 65,
+        legforgalmassabb : "ORD"
+    },
+    {
+        nev : 'Spirit Air Lines',
+        osszes :  224464,
+        latogatott : 52,
+        torolt :  2,
+        atlagos : 69,
+        legforgalmassabb : "FLL"
+    },
+    {
+        nev : 'PSA Airlines Inc.',
+        osszes :  313346,
+        latogatott : 104,
+        torolt :  2,
+        atlagos : 70,
+        legforgalmassabb : "CLT"
+    },
+    {
+        nev : 'SkyWest Airlines Inc.',
+        osszes :  909735,
+        latogatott : 277,
+        torolt :  2,
+        atlagos : 84,
+        legforgalmassabb : "ORD"
+    },
+    {
+        nev : 'United Air Lines Inc.',
+        osszes :  673640,
+        latogatott : 122,
+        torolt :  1,
+        atlagos : 75,
+        legforgalmassabb : "ORD"
+    },
+    {
+        nev : 'Southwest Airlines Co.',
+        osszes :  1463985,
+        latogatott : 90,
+        torolt :  2,
+        atlagos : 52,
+        legforgalmassabb : "MDW"
+    },
+    {
+        nev : 'Mesa Airlines Inc.',
+        osszes :  244057,
+        latogatott : 130,
+        torolt :  3,
+        atlagos : 81,
+        legforgalmassabb : "DFW"
+    },
+    {
+        nev : 'Republic Airline',
+        osszes :  360077,
+        latogatott : 120,
+        torolt :  2,
+        atlagos : 69,
+        legforgalmassabb : "LGA"
+    },
+    {
+        nev : 'ExpressJet Airlines LLC',
+        osszes :  119067,
+        latogatott : 120,
+        torolt :  4,
+        atlagos : 86,
+        legforgalmassabb : "ORD"
+    },
+
+
+]
